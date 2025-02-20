9594c66a672233676e0ea66121dcacb58c0d1fe5
GH767 <gavinh628@stemcollegiate.ca>
2025-02-20 09:14:00 -0700

    Create 2025-02-20.py

---
+++ 2025-02-20.py
@@ -0,0 +1,9 @@
+# though this lesson I learned that you can combine different varibuls with others.
+
+food = input("What is your favorite food? ")
+plant = input("What is your favorite edible plant? ")
+cooking = input("What is your favorite method of cooking? ")
+burned = input("What is a word that describes burned food? ")
+item = input("What is your favorite household item? " )
+word = input("What is your favouite word? ")
+print("So, you like " + food + " cooked with " + plant + " on a plate of " + item + "s" + " and its " + word + "!")
