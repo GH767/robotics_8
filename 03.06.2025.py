956097ed8b82babd1f2137d6d913244402b9f65b
GH767 <gavinh628@stemcollegiate.ca>
2025-03-27 14:18:23 -0600

    Create 03-06-2025.py

---
+++ 03.06.2025.py
@@ -0,0 +1,22 @@
+
+# today I learned how use if else in different ways.
+
+print("Which character are you from Avengers?'")
+print()
+print("Answer these questions and let's find out.")
+print()
+print("Please use Y or N for yes and no.")
+hero = input("Do you like heros?: ")
+if hero == "Y":
+  print("You must be a good person")
+IronIsCool = input("Do you think saving people is a good: ")  
+if IronIsCool == "Y":
+  print("you are a good hero")
+TimeTravel = input("Do you like traveling through time?: ")  
+if TimeTravel == "Y":
+  print("You must be dr.strange !")
+Strong = input("Are you super strong?: ")
+if Strong == "Y":
+  print("You have got to be hulk!")
+else:
+  print("I guess you might be captain america")
